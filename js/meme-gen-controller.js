91c7af8c99ead6a68e19b1af5ff9c2a4e6fd9326
ItayAvrahami <itayavrahami1@gmail.com>
2020-08-12 17:55:35 +0300

    gallery and canvas with text

--- js/meme-gen-controller.js
+++ js/meme-gen-controller.js
@@ -1,3 +1,81 @@
 'use strict';
 
-console.log('controller');
+var gCanvas = document.getElementById('myCanvas');
+var gCtx = gCanvas.getContext('2d');
+
+function init() {
+    renderGallery()
+}
+
+function renderGallery() {
+    var images = getImgs();
+
+    var strHTML = images.map(image => {
+        return `<img src="${image.url}" onclick="onImgClick(${image.id})" alt="">`
+    })
+    document.querySelector('.gallery').innerHTML = strHTML.join('');
+}
+
+function onImgClick(imgId) {
+    // shownig the canvas and hiding the gallery    
+    document.querySelector('.canvas-container').classList.add('shown')
+    document.querySelector('.gallery').classList.add('hidden');
+    
+    gCurrImg = getImgById(imgId);
+    getMeme(imgId);
+    //drawing the meme image to its place
+    drawImg(gCurrImg.url, drawText)
+}
+
+async function drawImg(imgUrl, func) {
+    const img = new Image();
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height);
+        func();
+    }
+    img.src = imgUrl;
+}
+
+function drawText() {
+    var memeLineProperties = gMeme['lines'][0];
+    var x = memeLineProperties.coord.x;
+    var y = memeLineProperties.coord.y;
+    var text = memeLineProperties['txt'];
+    var fontStr = memeLineProperties['size'] + 'px Impact';
+    gCtx.lineWidth = '1';
+    gCtx.strokeStyle = memeLineProperties['strok-color'];;
+    gCtx.fillStyle = memeLineProperties['fill-color'];
+    gCtx.font = fontStr;
+    gCtx.textAlign = memeLineProperties['align'];
+    gCtx.fillText(text, x, y);
+    gCtx.strokeText(text, x, y);
+}
+
+function onTxtToAdd(ev) {
+    var memeTxt = document.querySelector('.meme-txt').value;
+    var charToAdd = ev.key;
+    if (charToAdd.length > 1 && charToAdd !== 'Backspace') {
+        return
+    } else if (charToAdd === 'Backspace') {
+        memeTxt = memeTxt.substring(0, memeTxt.length - 1);
+    } else {
+        memeTxt += ev.key;
+    }
+    setMemeLine(memeTxt)
+    drawImg(gCurrImg.url, drawText)
+
+}
+
+
+// window.addEventListener('resize', function(){
+//     // gCanvas.width = window.innerWidth;
+//     // gCanvas.height = window.innerHeight;
+//     resizeCanvas()
+// })
+
+// function resizeCanvas() {
+//     const elContainer = document.querySelector('.canvas-container');
+//     // Note: changing the canvas dimension this way clears the canvas
+//     gCanvas.width = elContainer.offsetWidth;
+//     gCanvas.height = elContainer.offsetHeight;
+// }
--- js/meme-gen-service.js
+++ js/meme-gen-service.js
@@ -1,4 +1,85 @@
 'use strict';
 
-console.log('service');
+var gImgs;
+var gMeme;
+var gKeywords;
+var gId = 1;
+var gMeme;
+var gCurrImg;
+
+var defaultImgs = [
+    {
+        url: 'img/meme-imgs (square)/5.jpg',
+        keywords: ['baby, beach, success']
+    },
+    {
+        url: 'img/meme-imgs (square)/1.jpg',
+        keywords: ['Trump, angry, speech']
+    }
+]
+
+gImgs = createImgs(defaultImgs);
+
+
+
+function getImgs(){
+    return gImgs;
+}
+function getMeme(imgId){
+    var meme = creatMeme(imgId);
+    gMeme = meme;
+    return meme;
+}
+
+
+function getImgById(imgId){
+    var image = gImgs.find(image => {
+        return image.id === imgId;
+    })
+    gCurrImg = image;
+    return gCurrImg;
+}
+
+function creatMeme(imgId = 5) {
+    var meme = {
+        selectedImgId: imgId,
+        selectedLineIdx: 0,
+        lines: [
+            {
+                txt: 'Your Text',
+                size: 20,
+                align: 'left',
+                'fill-color': 'white',
+                'strok-color': 'red',
+                coord: {x: 200, y:200}
+            }
+        ]
+    }
+    return meme;
+}
+
+function createImgs(defaultImgs) {
+    var imgs = defaultImgs.map(image => {
+        return createImg(image)
+    })
+    gImgs = imgs;
+    return gImgs;
+}
+
+function createImg(image) {
+    var currImg = {
+        id: 5,
+        url: image.url,
+        keywords: image.keywords
+    }
+    return currImg;
+}
+
+function setMemeLine(line){
+    gMeme.lines[0].txt = line;   
+}
+
+function clearLine(){
+    setMemeLine('')
+}
 
